d93ce539079be2fca7db2493c2ce41498a014c93
Tailor1337 <luca-5@gmx.de>
2019-03-06 21:49:27 +0100

    First commit

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour {
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.collider.tag == "Gegner")
+        {
+            Debug.Log("Wand");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (collision.collider.tag == "Ziel")
+        {
+            Debug.Log("Ziel");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Kugel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kugel : MonoBehaviour {
+
+    public bool isFlat = true;
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+        if (isFlat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+        rb.AddForce(tilt * 12);
+    }
+
+}
